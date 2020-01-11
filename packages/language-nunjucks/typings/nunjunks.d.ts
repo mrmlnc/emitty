da9321ee70de95e6af38dd8e3665cb6de24d66b1
Alexey <lesha.ogonkov@gmail.com>
2020-01-11 17:26:22 +0300

    Extend Nunjuck typings

--- packages/language-nunjucks/typings/nunjunks.d.ts
+++ packages/language-nunjucks/typings/nunjunks.d.ts
@@ -1,18 +1,59 @@
-declare module "nunjucks" {
-	namespace nunjucks {
+declare module 'nunjucks' {
+	namespace nodes {
 		export type Node = {
-			children?: Node[];
-			template?: Node;
+			findAll<TType extends NunjucksNodes>(type: TType, results?: []): TType[];
 			value?: string;
-		}
+			children: NunjucksNodes[];
+		};
 
+		type NodeList = Node & {
+			children: NunjucksNodes[];
+		};
+
+		export type Root = NodeList & {
+		};
+
+		type TemplateRef = Node & {
+			template?: NunjucksNodes;
+		};
+
+		type Extends = TemplateRef & {
+		};
+
+		type Include = Node & {
+			template: NunjucksNodes;
+			ignoreMissing: boolean;
+		};
+
+		type Import = Node & {
+			template: NunjucksNodes;
+		};
+
+		type FromImport = Node & {
+			template: NunjucksNodes;
+		};
+
+		export type Nodes = {
+			Node: Node;
+			Root: Root;
+			Extends: Extends;
+			Include: Include;
+			Import: Import;
+			FromImport: FromImport;
+		};
+
+		export type NunjucksNodes = Node | Root | Extends | Include | Import | FromImport;
+	}
+
+	namespace nunjucks {
 		export type Parser = {
-			parse(content: string): Node;
+			parse(content: string): nodes.Root;
 		};
 	}
 
 	const nunjucks: {
 		parser: nunjucks.Parser;
+		nodes: nodes.Nodes;
 	};
 
 	export = nunjucks;
